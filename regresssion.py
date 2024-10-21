307c57c5dc6548eabe534b7de83231e4229c265d
santenana <santgaso@gmail.com>
2024-10-21 01:13:38 +0000

    se agreg requirements

--- regresssion.py
+++ regresssion.py
@@ -4,7 +4,6 @@ import plotly.express as px
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np  
-# import ipywidgets as widgets
 
 def main():
     data = st.file_uploader("📁 Load Data File", type=["csv", "xlsx"])
@@ -15,7 +14,6 @@ def main():
         viz = ['bar 📊', 'pie 🥧', 'correlation 🎢', 'Line 🪡', 'Trend 📈']
         tabs = st.tabs(viz)
         
-        # Selección del campo de fecha (puede ser "Year", "year", "Date", "date")
         date_columns = [col for col in columns if 'year' in col.lower() or 'date' in col.lower()]
         
         if len(date_columns) == 0:
@@ -25,39 +23,32 @@ def main():
         with tabs[0]:
             date_field = st.selectbox("Select Date Field for Bar Plot", date_columns)
             if date_field:
-                # Si el campo contiene 'year', tratarlo como año
                 if 'year' in date_field.lower():
                     df[date_field] = pd.to_numeric(df[date_field], errors='coerce')  # Asegurar que sea numérico
                     df = df.dropna(subset=[date_field])  # Eliminar valores NaN
                     min_year = int(df[date_field].min())
                     max_year = int(df[date_field].max())
                     
-                    # Slider para el rango de años
                     year_range = st.slider('Select Year range for Bar Plot',
                                            min_value=min_year, 
                                            max_value=max_year, 
                                            value=(min_year, max_year))
                     
-                    # Filtrar los datos por el rango seleccionado
                     df_filtered = df[(df[date_field] >= year_range[0]) & (df[date_field] <= year_range[1])]
                     
-                # Si el campo contiene 'date', tratarlo como fecha completa
                 elif 'date' in date_field.lower():
-                    df[date_field] = pd.to_datetime(df[date_field], errors='coerce')  # Convertir a fecha
-                    df = df.dropna(subset=[date_field])  # Eliminar valores NaT
+                    df[date_field] = pd.to_datetime(df[date_field], errors='coerce')  
+                    df = df.dropna(subset=[date_field])  
                     min_date = df[date_field].min().date()
                     max_date = df[date_field].max().date()
                     
-                    # Slider para el rango de fechas
                     year_range = st.slider('Select Date range for Bar Plot',
                                            min_value=min_date, 
                                            max_value=max_date, 
                                            value=(min_date, max_date))
                     
-                    # Filtrar los datos por el rango seleccionado
                     df_filtered = df[(df[date_field].dt.date >= year_range[0]) & (df[date_field].dt.date <= year_range[1])]
                 
-                # Selección de las variables para el gráfico
                 numeric_columns = df_filtered.select_dtypes(include='number').columns.tolist()
                 string_columns = df_filtered.select_dtypes(include='object').columns.tolist()
                 
@@ -65,7 +56,7 @@ def main():
                 param_2 = st.selectbox("Select Y value for Barplot", numeric_columns, placeholder="Select one parameter",index=None)
                 filter = st.selectbox("Select filter for Barplot", string_columns, placeholder="Select one parameter",index=None)
                 
-                if param_1 and param_2 and filter:
+                if param_1 and param_2 or filter:
                     fig, ax = plt.subplots()
                     sns.barplot(data=df_filtered, x=param_1, y=param_2, hue=filter, ax=ax, errorbar=None)
                     ax.set_title(f'Total {param_2} by {param_1} filtering {filter}', fontsize=14)
@@ -76,18 +67,15 @@ def main():
             else:
                 st.warning("Por favor, selecciona un campo de fecha válido.")
 
-        # Pestaña de pie chart
         with tabs[1]:
             date_field_pie = st.selectbox("Select Date Field for Pie Chart", date_columns)
             if date_field_pie:
-                # Tratar el campo como año o fecha, según su tipo
                 if 'year' in date_field_pie.lower():
                     df[date_field_pie] = pd.to_numeric(df[date_field_pie], errors='coerce')
                     df = df.dropna(subset=[date_field_pie])
                     min_year = int(df[date_field_pie].min())
                     max_year = int(df[date_field_pie].max())
                     
-                    # Slider para el rango de años
                     year_range_2 = st.slider('Select Year range for Pie Chart',
                                              min_value=min_year, 
                                              max_value=max_year, 
@@ -101,7 +89,6 @@ def main():
                     min_date = df[date_field_pie].min().date()
                     max_date = df[date_field_pie].max().date()
                     
-                    # Slider para el rango de fechas
                     year_range_2 = st.slider('Select Date range for Pie Chart',
                                              min_value=min_date, 
                                              max_value=max_date, 
@@ -109,7 +96,6 @@ def main():
                     
                     df_filtered = df[(df[date_field_pie].dt.date >= year_range_2[0]) & (df[date_field_pie].dt.date <= year_range_2[1])]
                 
-                # Selección de variables para el gráfico de pie
                 numeric_columns_2 = df_filtered.select_dtypes(include='number').columns.tolist()
                 string_columns_2 = df_filtered.select_dtypes(include='object').columns.tolist()
                 
